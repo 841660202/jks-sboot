4c8181635679b7b587cb0a43df4da34088862f4a
hailong.chen <chenhailong@fr-inc.cn>
2020-09-09 15:35:31 +0800

    init

---
+++ src/main/java/com/example/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 海龙
+ * @description
+ * @date 2020-09-09 15:16:01
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/say")
+    public String say() {
+        return "Hello World";
+
+    }
+}
